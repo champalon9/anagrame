932078607b5a5388a1bdfa3017a5c4f41d84b45e
Alexandru Pascariu <alexandrucarlo@live.com>
2022-05-04 01:04:53 +0300

    lowercase cuvinte_ro + gitignore

--- main.cpp
+++ main.cpp
@@ -15,19 +15,36 @@ int main()
    cin >> query;
    transform(query.begin(), query.end(), query.begin(), ::tolower); // transformam in litere mici
 
+   int apQuery[26], apCuv[26];
+   fill(apQuery, apQuery + 26, 0);
+   for (char &c : query)
+      if (isalpha(c))
+         apQuery[c - 'a'] ++;
+
    // luam cuvintele din fisier care contin macar o litera din query
+   // agares = query
+   // aaaa = cuvant
+
    fin.open("cuvinte_ro.txt", ios::in);
    string cuvant;
    while (fin >> cuvant)
    {
-      bool pastram = false; // aa a fa fga
-      for (char &c : query)
-         if (cuvant.find_first_of(c) != -1)
-            pastram = true;
+      fill(apCuv, apCuv + 26, 0);
+      for (char &c : cuvant)
+         apCuv[c - 'a'] ++;
+
+      bool pastram = true;
+      for (int i = 0; i < 26; ++i)
+         if (apCuv[i] > apQuery[i])
+         {
+            pastram = false;
+            break;
+         }
 
       if (pastram)
          cuv.push_back(cuvant);
    }
-      
-   cout << cuv.size();
+   
+   for (int i = 0; i < 20; ++i)
+      cout << cuv[i] << '\n';
 }
